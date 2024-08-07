3cac3f1043e6b7f10004e5cb36ac473f3cf59050
Thanush K <thanushgowdakvht@gmail.com>
2024-08-07 13:54:46 +0530

    python

---
+++ add.py
@@ -0,0 +1,30 @@
+import sys 
+# Get command line arguments as strings 
+arg1 = sys.argv[1] 
+arg2 = sys.argv[2] 
+# Convert arguments to integers and compute sum 
+sum = int(arg1) + int(arg2) 
+# Print the result 
+print("The sum of", arg1, "and", arg2, "is", sum) 
+#run in command prompt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
